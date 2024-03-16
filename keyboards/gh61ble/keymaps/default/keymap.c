74f116e620452b185398085aba1bffa3bcb78d08
hitsmaxft <mfthits@gmail.com>
2024-03-16 08:10:08 +0800

    Refactor: Update gh61ble keyboard configuration
     • Updated bootloader and processor info in keyboard.json.
     • Added community layouts to keyboard.json.
     • Modified key matrix and positions in keyboard.json.
     • Adjusted layer 0 keymap to match 60% ANSI layout.
     • Updated layer 1 and 2 keymaps to support additional functions and macros.
    
    use layout macro for key remapping

--- keyboards/gh61ble/keymaps/default/keymap.c
+++ keyboards/gh61ble/keymaps/default/keymap.c
@@ -3,6 +3,7 @@
 
 #include QMK_KEYBOARD_H
 
+
 // clang-format off
 /* QWERTY
  * .----00---------01---------02---------03---------04---------05---------06---------07---------08---------09---------10---------11---------12----.
@@ -20,27 +21,28 @@
  */
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = {
-        { KC_ESC,     KC_1,       KC_2,       KC_3,      KC_4,       KC_5,     KC_6,      KC_7,      KC_8,     KC_9,      KC_0,     KC_MINS,   KC_EQL},
-        { KC_TAB,     KC_Q,       KC_W,       KC_E,      KC_R,       KC_T,     KC_Y,      KC_U,      KC_I,     KC_O,      KC_P,     KC_LBRC,   KC_RBRC},
-        { KC_CAPS,    KC_A,       KC_S,       KC_D,      KC_F,       KC_G,     KC_H,      KC_J,      KC_K,     KC_L,      KC_SCLN,  KC_QUOT,   KC_ENT},
-        { KC_LSFT,    KC_Z,       KC_X,       KC_C,      KC_V,       KC_B,     KC_N,      KC_M,      KC_COMM,  KC_DOT,    KC_SLSH,  KC_UP,     KC_DEL},
-        { KC_LCTL,    KC_LGUI,    KC_LALT,    KC_SPC,    KC_RALT,    MO(1),    KC_LEFT,   KC_DOWN,   KC_RGHT,  KC_BSPC,   KC_BSLS,  KC_RSFT,   KC_NO  }
-    },
-    [1] = {
-        { QK_BOOT,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,},
-        { KC_NO,    KC_F23,  KC_F19,  KC_F20,  KC_F18,  /*BLE_DEL*/KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,},
-        { KC_NO,    KC_NO,   RGB_MODE_FORWARD,   RGB_TOG,   SAFE_RANGE,    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,},
-        { KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,},
-        { KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,}
-    },
-    [2] = {
-        { KC_ESC,  KC_1,   KC_2,   KC_3,   KC_4,      KC_5,   KC_6,  KC_7,   KC_8,   KC_9,    KC_0,    KC_MINS, KC_EQL},
-        { KC_TAB,  KC_Q,   KC_W,   KC_E,   KC_R,      KC_T,  KC_Y,   KC_U,   KC_I,   KC_O,    KC_P,    KC_LBRC, KC_RBRC},
-        { KC_CAPS, KC_A,   KC_S,   KC_D,   KC_F,      KC_G,  KC_H,   KC_J,   KC_K,   KC_L,    KC_SCLN, KC_QUOT, KC_ENT},
-        { KC_LSFT, KC_Z,   KC_X,   KC_C,   KC_V,      KC_B,  KC_N,   KC_M,   KC_COMM,KC_DOT,  KC_SLSH, KC_RSFT, KC_UP},
-        { KC_LCTL ,KC_LGUI,KC_LALT,KC_SPC, KC_RALT,   MO(1),  KC_LEFT, KC_DOWN, KC_RGHT,  KC_BSPC, KC_BSLS,KC_NO,KC_NO }
-    }
+
+    [0] = LAYOUT_60_ansi(
+         KC_ESC,     KC_1,       KC_2,       KC_3,      KC_4,       KC_5,     KC_6,      KC_7,      KC_8,     KC_9,      KC_0,     KC_MINS,   KC_EQL,KC_BSPC ,
+         KC_TAB,     KC_Q,       KC_W,       KC_E,      KC_R,       KC_T,     KC_Y,      KC_U,      KC_I,     KC_O,      KC_P,     KC_LBRC,   KC_RBRC, KC_BSLS,
+         KC_CAPS,    KC_A,       KC_S,       KC_D,      KC_F,       KC_G,     KC_H,      KC_J,      KC_K,     KC_L,      KC_SCLN,  KC_QUOT,   KC_ENT,
+         KC_LSFT,    KC_Z,       KC_X,       KC_C,      KC_V,       KC_B,     KC_N,      KC_M,      KC_COMM,  KC_DOT,    KC_SLSH,   KC_DEL,
+         KC_LCTL,    KC_LGUI,    KC_LALT,    KC_SPC,    KC_RALT,    MO(1),    MO(2), KC_RCTL
+    ),
+    [1] = LAYOUT_60_ansi(
+         QK_BOOT,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO, KC_NO,
+         KC_NO,    KC_F23,  KC_F19,  KC_F20,  KC_F18,  /*BLE_DEL*/KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,
+         KC_NO,    KC_NO,   RGB_MODE_FORWARD,   RGB_TOG,   SAFE_RANGE,    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,
+         KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,
+         KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,  KC_NO,  KC_NO
+    ),
+    [2] = LAYOUT_60_ansi(
+         KC_ESC,  KC_1,   KC_2,   KC_3,   KC_4,      KC_5,   KC_6,  KC_7,   KC_8,   KC_9,    KC_0,    KC_MINS, KC_EQL, KC_BSPC,
+         KC_TAB,  KC_Q,   KC_W,   KC_E,   KC_R,      KC_T,  KC_Y,   KC_U,   KC_I,   KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
+         KC_CAPS, KC_A,   KC_S,   KC_D,   KC_F,      KC_G,  KC_H,   KC_J,   KC_K,   KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
+         KC_LSFT, KC_Z,   KC_X,   KC_C,   KC_V,      KC_B,  KC_N,   KC_M,   KC_COMM,KC_DOT,  KC_SLSH, KC_RSFT,
+         KC_TRNS ,KC_TRNS,KC_TRNS,KC_TRNS, KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS
+    ),
 };
 
 
