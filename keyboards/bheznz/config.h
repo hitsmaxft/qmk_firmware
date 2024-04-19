c5c617a6f594fac575601ed83946ae8a0c79ebd8
hitsmaxft <mfthits@gmail.com>
2024-04-19 18:54:03 +0800

    znz

---
+++ keyboards/bheznz/config.h
@@ -0,0 +1,13 @@
+#pragma once
+
+
+
+#define WS2812_DI_PIN B0
+#define WS2812_PWM_DRIVER PWMD3
+#define WS2812_PWM_CHANNEL 3
+#define WS2812_PWM_FREQUENCY
+
+#define RGBLIGHT_LED_COUNT 16
+#define RGB_MATRIX_LED_COUNT 16
+
+#include_next "config.h"
