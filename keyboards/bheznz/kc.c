1a2e1d5b2262f6bee2d50f17eed838edfe689a06
hitsmaxft <mfthits@gmail.com>
2024-05-04 23:21:36 +0800

    K44: Keycode to ASCII mapping update, add navigation keys, keymap layout changes

--- keyboards/bheznz/kc.c
+++ keyboards/bheznz/kc.c
@@ -2,14 +2,101 @@
 #include "quantum.h"
 #include "bheznz.h"
 
+char* keycode_to_ascii(uint16_t keycode) {
+    static char str[5];
+
+    switch (keycode) {
+        case 0x0000: return "NO";
+        case 0x0001: return "TRAN";
+        case 0x0004: return "A";
+        case 0x0005: return "B";
+        case 0x0006: return "C";
+        case 0x0007: return "D";
+        case 0x0008: return "E";
+        case 0x0009: return "F";
+        case 0x000A: return "G";
+        case 0x000B: return "H";
+        case 0x000C: return "I";
+        case 0x000D: return "J";
+        case 0x000E: return "K";
+        case 0x000F: return "L";
+        case 0x0010: return "M";
+        case 0x0011: return "N";
+        case 0x0012: return "O";
+        case 0x0013: return "P";
+        case 0x0014: return "Q";
+        case 0x0015: return "R";
+        case 0x0016: return "S";
+        case 0x0017: return "T";
+        case 0x0018: return "U";
+        case 0x0019: return "V";
+        case 0x001A: return "W";
+        case 0x001B: return "X";
+        case 0x001C: return "Y";
+        case 0x001D: return "Z";
+        case 0x001E: return "1";
+        case 0x001F: return "2";
+        case 0x0020: return "3";
+        case 0x0021: return "4";
+        case 0x0022: return "5";
+        case 0x0023: return "6";
+        case 0x0024: return "7";
+        case 0x0025: return "8";
+        case 0x0026: return "9";
+        case 0x0027: return "0";
+        case 0x0028: return "ENT";
+        case 0x0029: return "ESC";
+        case 0x002A: return "BSPC";
+        case 0x002B: return "TAB";
+        case 0x002C: return "SPC";
+        case 0x002D: return "-";
+        case 0x002E: return "=";
+        case 0x002F: return "[";
+        case 0x0030: return "]";
+        case 0x0031: return "\\";
+        case 0x0032: return "#";
+        case 0x0033: return ";";
+        case 0x0034: return "'";
+        case 0x0035: return "`";
+        case 0x0036: return ",";
+        case 0x0037: return ".";
+        case 0x0038: return "/";
+        case 0x0039: return "CAPS";
+        case 0x003A: return "F1";
+        case 0x003B: return "F2";
+        case 0x003C: return "F3";
+        case 0x003D: return "F4";
+        case 0x003E: return "F5";
+        case 0x003F: return "F6";
+        case 0x0040: return "F7";
+        case 0x0041: return "F8";
+        case 0x0042: return "F9";
+        case 0x0043: return "F10";
+        case 0x0044: return "F11";
+        case 0x0045: return "F12";
+        case 0x0046: return "PSCR";
+        case 0x0047: return "SLCK";
+        case 0x0048: return "PAUS";
+        case 0x0049: return "INS";
+        case 0x004A: return "HOME";
+        case 0x004B: return "PGUP";
+        case 0x004C: return "DEL";
+        case 0x004D: return "END";
+        case 0x004E: return "PGDN";
+        case 0x004F: return "RGT";
+        case 0x0050: return "LFT";
+        case 0x0051: return "DWN";
+        case 0x0052: return "UP";
+        default: str[0] = ' '; str[1] = ' '; return str; // Unknown keycode
+    }
+}
 
 /** a simple keycode to ascii converter **/
 
-char* keycode_to_ascii(uint16_t keycode) {
+char* keycode_to_ascii2(uint16_t keycode) {
 
     //reuse char array
-    static char ascii[3] = {' ', ' ', '\0'}; // 初始化为全空字符串
-        ascii[0] = ' ';
+    static char ascii[4] = { ' ', ' ', '\0'}; // 初始化为全空字符串
     if (keycode >= KC_A && keycode <= KC_Z) {
         ascii[1] = 'A' + ((keycode - KC_A) % 26);
     } else if (keycode >= KC_1 && keycode <= KC_9) {
@@ -37,7 +124,7 @@ char* keycode_to_ascii(uint16_t keycode) {
             case KC_DOT:
             case KC_SLASH:
             case KC_CAPS_LOCK:
-                ascii[1] = (char[]){'n', 0x1B, 0x08, 't', ' ', '-', '=', '[', ']', '\\', '#', ';', '\'', '`', ',', '.', '/'}[keycode - KC_ENTER];
+                ascii[1] = (char[]){'n', 'E', 'S', 't', ' ', '-', '=', '[', ']', '\\', '#', ';', '\'', '`', ',', '.', '/'}[keycode - KC_ENTER];
                 break;
             case RGB_TOG:
                 return "RGBT";
@@ -71,6 +158,24 @@ char* keycode_to_ascii(uint16_t keycode) {
                 break;
             case QK_DEBUG_TOGGLE:
                 return "DEBG";
+                break;
+            case KC_UP:
+                return "UP";
+                break;
+            case KC_DOWN:
+                return "DWN";
+                break;
+            case KC_LEFT:
+                return "<-";
+                break;
+            case KC_RIGHT:
+                return "->";
+                break;
+            case KC_LEFT_CTRL:
+                return "CTL";
+                break;
+            case KC_LEFT_SHIFT:
+                return "SHT";
                 break;
             default:
                 ascii[0] = '-'; // 对于不表示打印字符的按键，返回空字符串
--- keyboards/bheznz/keymaps/k44/keymap.c
+++ keyboards/bheznz/keymaps/k44/keymap.c
@@ -16,21 +16,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * └───┴───┴───┴───┘
      */
     [0] = LAYOUT_ortho_4x4(
+        _______,    KC_UP  ,    KC_SPC,         KC_A,
+        KC_LEFT,    KC_ENTER,   KC_RIGHT,       KC_B,
+        KC_LEFT_CTRL,    KC_DOWN,    KC_LEFT_SHIFT,        KC_C,
+        LT(1, KC_Z),    KC_0,   LT(2, KC_C),    KC_D
+    ),
+    [1] = LAYOUT_ortho_4x4(
+        RGB_SPI,    RGB_VAI,    RGB_HUI,    _______,
+        RGB_SPD,    RGB_VAD,    RGB_HUD,    _______,
+        RGB_MOD,    RGB_TOG,    _______,    KC_ZNZ_DEBUG,
+        KC_NO,      QK_BOOT,    _______,   QK_DEBUG_TOGGLE
+    ),
+    [2] = LAYOUT_ortho_4x4(
         KC_1,    KC_2,    KC_3,    KC_A,
         KC_4,    KC_5,    KC_6,    KC_B,
         KC_7,    KC_8,    KC_9,    KC_C,
-        LT(1, KC_Z),    KC_0,    LT(2, KC_C),    KC_D
-    ),
-    [1] = LAYOUT_ortho_4x4(
-        RGB_SPI,    RGB_VAI,    RGB_HUI,    RGB_HUD,
-        RGB_SPD,    RGB_VAD,    RGB_HUD,    RGB_RMOD,
-        RGB_MOD,    RGB_TOG,    _______,    KC_ZNZ_DEBUG,
-        QK_BOOT,    RGB_TOG,    RGB_SPI,   QK_DEBUG_TOGGLE
-    ),
-    [2] = LAYOUT_ortho_4x4(
-        _______,    _______,    _______,    KC_ZNZ_DEBUG,
-        _______,    _______,    _______,    KC_ZNZ_DEBUG,
-        _______,    _______,    _______,    KC_ZNZ_DEBUG,
-        _______,    _______,    RGB_SPI,   RGB_SPD
+        _______, KC_0,    KC_NO,   KC_D
     ),
 };
