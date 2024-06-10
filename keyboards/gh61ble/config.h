6e0832f63a4f8f55f1a050f184b988894465348e
hitsmaxft <mfthits@gmail.com>
2024-06-10 23:46:52 +0800

    fix

--- keyboards/gh61ble/config.h
+++ keyboards/gh61ble/config.h
@@ -19,4 +19,3 @@
 #define WS2812_PWM_DMA_CHANNEL       5                      // DMA Channel for TIMx_UP, see the respective reference manual for the appropriate values for your MCU.
 
 //#define WS2812_PWM_DMAMUX_ID         STM32_DMAMUX1_TIM3_UP  // DMAMUX configuration for TIMx_UP -- only required if your MCU has a DMAMUX peripheral, see the respective reference manual for the appropriate values for your MCU.
-
